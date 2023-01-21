80e4ca09ed89684a026f0eb775c063d907e0ea89
shcaba <jason.cope@noaa>
2023-01-20 16:12:50 -0800

    Add materials for Gitpage

--- Bridge model/WA_SB_Dep_Comp_plots.r
+++ Bridge model/WA_SB_Dep_Comp_plots.r
@@ -1,13 +1,14 @@
 library(ggplot2)
 
-WA_comps<-read.csv("C:/Users/Jason.Cope/Desktop/Black rockfish 2022/Models/WA/Bridge/WA_SB_Dep_comps.csv")
+WA_comps<-read.csv("C:/Users/Jason.Cope/Documents/Github/Sebastes_melanops_WA/Bridge model/WA_SB_Dep_comps.csv")
 WA_comps$SSversion<-as.factor(WA_comps$SSversion)
 ggplot(WA_comps,aes(Year,SB,group=SSversion,fill=SSversion))+
   geom_line(aes(color=SSversion,linetype=SSversion),lwd=1.25)+
   ylim(0,1500)+
   geom_ribbon(aes(ymin=low_SB,ymax=hi_SB),alpha=0.1)+
   theme_bw()+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  ylab("Spawning Output")
 
 ggplot(WA_comps,aes(Year,Depletion,group=SSversion,fill=SSversion))+
   geom_line(aes(color=SSversion,linetype=SSversion),lwd=1.25)+
