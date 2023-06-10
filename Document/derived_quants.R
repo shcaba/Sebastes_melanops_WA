bf58566e695f1c9cbd7794992875e64de4c63e6d
shcaba <jason.cope@noaa>
2023-06-09 19:39:59 -0700

    update ES and derived quants

--- Document/derived_quants.R
+++ Document/derived_quants.R
@@ -6,18 +6,18 @@ ssb0 = format(model$derived_quants[model$derived_quants$Label == "SSB_Virgin", 2
 ssb_targ = round(0.25 * model$derived_quants[model$derived_quants$Label == "SSB_Virgin", 2], 0)
 
 # Final Year SSB
-ssb_2021 = format(model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr), 2], digits = 2, scientific = FALSE, big.mark = ",") 
-low = model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr), 2] - qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr), 3]
-hi = model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr), 2] + qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr), 3]
-ssb_2021_low = format(low, digits = 2, scientific = FALSE, big.mark = ",") 
-ssb_2021_hi = format(hi, digits = 2, scientific = FALSE, big.mark = ",") 
+ssb_curr = format(model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr+1), 2], digits = 2, scientific = FALSE, big.mark = ",") 
+low = model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr+1), 2] - qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr+1), 3]
+hi = model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr+1), 2] + qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("SSB_",endyr+1), 3]
+ssb_curr_low = format(low, digits = 2, scientific = FALSE, big.mark = ",") 
+ssb_curr_hi = format(hi, digits = 2, scientific = FALSE, big.mark = ",") 
 
 # Final Year Depletion
-depl_2021 = round(model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr), 2], digits = 2) 
-low = model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr), 2] - qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr), 3]
-hi = model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr), 2] + qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr), 3]
-depl_2021_low = round(low, digits = 2) 
-depl_2021_hi  = round(hi,  digits = 2) 
+depl_curr = round(model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr+1), 2], digits = 2) 
+low = model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr+1), 2] - qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr+1), 3]
+hi = model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr+1), 2] + qnorm(1-(1-0.95)/2) * model$derived_quants[model$derived_quants$Label == paste0("Bratio_",endyr+1), 3]
+depl_curr_low = round(low, digits = 2) 
+depl_curr_hi  = round(hi,  digits = 2) 
 
 start = which(model$derived_quants$Label == paste0("Bratio_",startyr+1))
 end = which(model$derived_quants$Label == paste0("Bratio_",endyr))
@@ -29,7 +29,7 @@ depl_min_yr = substring(all[min[1], "Label"], 8)
 
 # SPR
 start = which(model$derived_quants$Label == paste0("SPRratio_",startyr+1))
-end = which(model$derived_quants$Label == paste0("SPRratio_",endyr-1))
+end = which(model$derived_quants$Label == paste0("SPRratio_",endyr))
 all = model$derived_quants[start:end, c("Label","Value")]
 max = sort(all$Value, index.return = TRUE, decreasing = TRUE)$ix
 spr_max = round(all[max[1], "Value"],2)
@@ -40,7 +40,7 @@ spr_recent_10 = round(all[grab:nrow(all), "Value"],2)
 
 # Exploitation
 start = which(model$derived_quants$Label == paste0("F_",startyr))
-end = which(model$derived_quants$Label == paste0("F_",endyr-1))
+end = which(model$derived_quants$Label == paste0("F_",endyr))
 all = model$derived_quants[start:end, c("Label","Value")]
 grab = which(all$Label == paste0("F_",endyr-10))
 exploit_recent_10 = round(all[grab:nrow(all), "Value"],2)
